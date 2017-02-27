15cdec53364757df63aa6de78b1a1d6a8aefb52c
Marouane90 <marouane.ammar.90@gmail.com>
2017-02-27 16:52:50 +0100

    product

---
+++ apps/product.php
@@ -0,0 +1,25 @@
+<?php
+if(isset($_GET['id']))
+{
+	$manager = new ProductsManager($db);
+	$products = $manager->findById($_GET['id']);
+// $id = intval($_GET['id']);
+// $res = mysqli_query($db, "SELECT articles.*,user.login FROM  articles , user WHERE user.id=articles.id_author AND articles.id=".$id);
+// $article = mysqli_fetch_assoc($res);
+	if ($products)
+	{	
+		var_dump($products);
+		require('views/product.phtml');
+	}
+	else
+	{
+		$errors[] = "L'article n'existe pas";
+		require('apps/errors.php');	
+	}	
+}
+else
+{
+	$errors[] = "L'article n'existe pas";
+	require('apps/errors.php');	
+}
+?>
