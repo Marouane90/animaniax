32911892792f6e14af5009462d6d0822da04ec70
mimimolette <maudfroehlicher@ymail.com>
2017-02-28 13:14:21 +0100

    user.php

---
+++ apps/user.php
@@ -0,0 +1,15 @@
+<?php
+
+if (isset($_SESSION['email']))
+{
+	$manager = new UserManager($db);
+	$user = $manager->findByEmail($_SESSION['email']);
+	require("views/user.phtml");
+}
+
+else {
+	echo "erreur";
+}
+
+
+?>
