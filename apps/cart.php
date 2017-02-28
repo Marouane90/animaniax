dfb685016a276bd71e384d2d80a415534acbc824
titineo <laetitia.werle@yahoo.fr>
2017-02-28 15:06:11 +0100

    divers php

--- apps/cart.php
+++ apps/cart.php
@@ -1,4 +1,15 @@
 <?php 
-require("views/cart.phtml");
+if (isset($_SESSION['id']))
+{
+	$userManager = new UserManager($db);
+	$user = $userManager->findById($_SESSION['id']);
+	$manager = new OrdersManager($db);
+	$cart = $manager->findCartByUsers($user);
+	var_dump($cart);
+	require("views/cart.phtml");
+}
 
+else {
+	echo "Vous devez être connecté pour afficher le panier";
+}
 ?>
