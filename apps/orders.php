4bc11a9ca6b5aa1995d558ea1af74e899fd8a42b
titineo <laetitia.werle@yahoo.fr>
2017-02-28 14:34:05 +0100

    ordersPHP ordersPHTML userPHTML v12

--- apps/orders.php
+++ apps/orders.php
@@ -1,4 +1,13 @@
 <?php 
-require("views/orders.phtml");
+if (isset($_GET['id']))
+{
+	$manager = new OrdersManager($db);
+	$order = $manager->findById($_GET['id']);
+	require("views/orders.phtml");
+}
+
+else {
+	echo "Pas de commandes en cours!";
+}
 
 ?>
