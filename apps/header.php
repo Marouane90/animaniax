08915b12aa8ada3982ac26b9101b260738ca9af4
mimimolette <maudfroehlicher@ymail.com>
2017-03-01 10:01:55 +0100

    header classs active

--- apps/header.php
+++ apps/header.php
@@ -1,4 +1,47 @@
 <?php
+$page_cat = '';
+$page_nllecat = '';
+$page_nvprod = '';
+$page_profile = '';
+$page_logout = '';
+$page_login = '';
+$page_register = '';
+
+if ($page == 'categories')
+{
+	$page_cat = "active";
+
+}
+if ($page == 'create_category')
+{
+	$page_nllecat = "active";
+
+}
+if ($page == 'create_product')
+{
+	$page_nvprod = "active";
+
+}
+if ($page == 'user')
+{
+	$page_profile = "active";
+
+}
+if ($page == 'logout')
+{
+	$page_logout = "active";
+
+}
+if ($page == 'login')
+{
+	$page_login = "active";
+
+}
+if ($page == 'register')
+{
+	$page_register = "active";
+
+}
 if (isset($_SESSION['id']))
 {
 	if (isset($_SESSION['admin']) && $_SESSION['admin'] == true)
