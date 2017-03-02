1d61347be48acf4b18504bf7977f7090c4356da5
hansmanng <hansmann.g@gmail.com>
2017-03-02 10:16:32 +0100

    update product

--- apps/cart.php
+++ apps/cart.php
@@ -1,12 +1,9 @@
 <?php 
-// if (isset($_SESSION['id']))
-// {
-	// $userManager = new UserManager($db);
-	// $user = $userManager->findById($_SESSION['id']);
-	$manager = new ProductsManager($db);
-	$product = $manager->findById($_GET['id'])
-	$cart = $product->getCart();
-var_dump($product);
+if (isset($_SESSION['id']))
+{
+	$userManager = new UserManager($db);
+	$user = $userManager->findById($_SESSION['id']);
+	$cart = $user->getCart();
 	if ($cart) 
 	{
 		require("views/cart.phtml");
@@ -16,7 +13,7 @@ var_dump($product);
 		$errors[] = "Vous n'avez pas encore de panier en cours";
 		require('views/errors.phtml');
 	}
-// }
+}
 else
 {
 	$errors[] = "Vous devez être connecté pour afficher le panier";
