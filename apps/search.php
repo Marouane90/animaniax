864208c356f29d16e95a00020b8c8ab1995e5f0d
titineo <laetitia.werle@yahoo.fr>
2017-03-02 17:00:27 +0100

    searchPHP

--- apps/search.php
+++ apps/search.php
@@ -1,3 +1,16 @@
 <?php
-// 
+if(isset($_GET['search'])) 
+{   
+  $recherche = mysqli_real_escape_string($db, $_GET["search"]);
+  $res = mysqli_query($db, "SELECT * FROM categories WHERE name LIKE '%".$recherche."%' OR description LIKE '%".$recherche."%'");
+  while($category = mysqli_fetch_object($res, "Category", [$db]))
+  {
+    require ("views/search_category.phtml");
+  }
+  $res = mysqli_query($db, "SELECT * FROM products WHERE name LIKE '%".$recherche."%' OR description LIKE '%".$recherche."%'");
+  while($product = mysqli_fetch_object($res, "Products", [$db]))
+  {
+    require ("views/search_product.phtml");
+  }
+}
 ?>
