b3c36b2a5656fb955f7bfa7cf7c1a292b50b377e
titineo <laetitia.werle@yahoo.fr>
2017-03-02 10:44:22 +0100

    gestion erreur productPHP

--- apps/product.php
+++ apps/product.php
@@ -12,49 +12,15 @@ if (isset($_GET['id']))
 	else
 	{
 		require("views/product.phtml");
+		if (isset($_SESSION['id']))
+		{
+			require('apps/create_comment.php');
+		}
+		require('apps/comments.php'); 
 	}
 }
-else {
+else 
+{
 	echo "erreur";
 }
-
-
-if (isset($_SESSION['id']) || isset($_SESSION['admin']))
-{
-
-require('apps/create_comment.php');
-	
-}
-
-require('apps/comments.php'); 
-
-
-
-
-
-
-
-// if(isset($_GET['id']))
-// {
-// 	$manager = new ProductsManager($db);
-// 	$products = $manager->findById($_GET['id']);
-// // $id = intval($_GET['id']);
-// // $res = mysqli_query($db, "SELECT articles.*,user.login FROM  articles , user WHERE user.id=articles.id_author AND articles.id=".$id);
-// // $article = mysqli_fetch_assoc($res);
-// 	if ($products)
-// 	{	
-// 		var_dump($products);
-		// require('views/product.phtml');
-// 	}
-// 	else
-// 	{
-// 		$errors[] = "L'article n'existe pas";
-// 		require('apps/errors.php');	
-// 	}	
-// }
-// else
-// {
-// 	$errors[] = "L'article n'existe pas";
-// 	require('apps/errors.php');	
-// }
 ?>
