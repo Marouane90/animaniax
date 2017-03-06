4ed6b98f7228eea14d441ef9e3779ebd3ba18a0d
mimimolette <maudfroehlicher@ymail.com>
2017-03-06 13:57:44 +0100

    index nlle modif bla

--- index.php
+++ index.php
@@ -1,31 +1,24 @@
 <?php
 $errors = [];
 $page = "categories";
+// Live test : http://192.168.1.95/partage
 $db = mysqli_connect("192.168.1.57","animaniax","animaniax","animaniax");
-session_start();// http://php.net/manual/fr/function.session-start.php
-$access = ["errors", "login", "register", "categories", "create_category", "cart", "orders", "user", "create_product", "contact", "search", "products","product",];
+session_start();
+$access = ["errors", "login", "register", "categories", "create_category", "cart", "orders", "user", "create_product", "contact", "search", "products","product"];
 if (isset($_GET['page']) && in_array($_GET['page'], $access))
 {
     $page = $_GET['page'];
 }
-
-require('models/Exceptions.class.php');
-require('models/User.class.php');
-require('models/Category.class.php');
-require('models/Comment.class.php');
-require('models/Products.class.php');
-require('models/Orders.class.php');
-
-require('models/UserManager.class.php');
-require('models/CategoryManager.class.php');
-require('models/CommentManager.class.php');
-require('models/ProductsManager.class.php');
-require('models/OrdersManager.class.php');
-
-require('apps/traitement_category.php');
-require('apps/traitement_users.php');
-require('apps/traitement_comments.php');
-require('apps/traitement_Products.php');
-require('apps/traitement_orders.php');
+function __autoload($classname)// http://php.net/manual/fr/function.autoload.php
+{
+	require('models/'.$classname.'.class.php');
+}
+$access_traitement = ["login"=>"users", "register"=>"users", "create_category"=>"category", "cart"=>"orders",
+						"user"=>"users", "create_product"=>"Products", "product"=>"orders"]; // comments
+if (isset($_GET['page'], $access_traitement[$_GET['page']]))
+{
+	$traitement = $access_traitement[$_GET['page']];
+	require('apps/traitement_'.$traitement.'.php');
+}
 require('apps/skel.php');
 ?>
